e9b56e55eecb9f5d59f16b566d810c1e05a31aaa
Wei Fu <wefu@redhat.com>
2021-09-19 14:13:15 +0800

    sifive_cache: add flush_range func in sifive cache driver
    Signed-off-by: Wei Fu <wefu@redhat.com>

--- arch/riscv/lib/sifive_cache.c
+++ arch/riscv/lib/sifive_cache.c
@@ -25,3 +25,24 @@ void enable_caches(void)
 			log_debug("ccache enable failed");
 	}
 }
+
+
+void flush_dcache_range(unsigned long start, unsigned long end)
+{
+	struct udevice *dev;
+	int ret;
+
+	/* Enable ways of ccache */
+	ret = uclass_get_device_by_driver(UCLASS_CACHE,
+					  DM_DRIVER_GET(sifive_ccache),
+					  &dev);
+	if (ret) {
+		log_debug("Cannot flush dcache in %p - %p",
+			  (void *)start, (void *)end);
+	} else {
+		ret = flush_range(dev, start, end);
+		if (ret)
+			log_debug("ccache flush failed in %p - %p",
+				  (void *)start, (void *)end);
+	}
+}
