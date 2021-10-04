cdba6c4bd47447eecdf4905196bc059b5db7b22b
woonjiet.chong <woonjiet.chong@starfivetech.com>
2021-10-04 17:16:54 +0800

    Add DDR driver framework
    Reassign address for SPL STACK to avoid global data being wiped off during mem_malloc_init
    Remove unused config CONFIG_SPL_LOAD_FIT_ADDRESS
    Signed-off-by: woonjiet.chong <woonjiet.chong@starfivetech.com>

--- board/starfive/dubhe_fpga/dubhe_fpga_spl.c
+++ board/starfive/dubhe_fpga/dubhe_fpga_spl.c
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: (GPL-2.0 OR MIT)
 /* Copyright (c) 2021 StarFive Technology Co., Ltd. */
 
+#include <dm.h>
 #include <spl.h>
 
 int spl_board_init_f(void)
 {
+	int ret;
+	struct udevice *dev;
+
+	/* Init DRAM */
+	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (ret) {
+		debug("DRAM init failed: %d\n", ret);
+		return ret;
+	}
+
 	return 0;
 }
 
