eb119d1553cfbc3fbccba4b46bdfcd25a4abd54e
Yanhong Wang <yanhong.wang@linux.starfivetech.com>
2022-10-13 09:37:27 +0800

    arch: riscv: jh7110: read ram size from eeprom
    Update memory size information, which read from eeprom.
    
    Signed-off-by: Yanhong Wang <yanhong.wang@linux.starfivetech.com>

--- arch/riscv/cpu/jh7110/dram.c
+++ arch/riscv/cpu/jh7110/dram.c
@@ -4,6 +4,7 @@
  */
 
 #include <common.h>
+#include <asm/arch/eeprom.h>
 #include <fdtdec.h>
 #include <init.h>
 #include <linux/sizes.h>
@@ -12,12 +13,39 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int dram_init(void)
 {
-	return fdtdec_setup_mem_size_base();
+	int ret;
+	u32 data;
+	u32 len;
+	u32 offset;
+
+	data = 0;
+	len = 4;
+	offset = 88; /*offset of memory size stored in eeprom*/
+	ret = fdtdec_setup_mem_size_base();
+	if (ret)
+		goto err;
+
+	/*read memory size info*/
+	ret = get_data_from_eeprom(offset, len, (u8 *)&data);
+	if (ret == len)
+		gd->ram_size = (phys_size_t)(hextoul((char *)&data, NULL) & 0xff) << 30;
+	ret = 0;
+
+err:
+	return ret;
 }
 
 int dram_init_banksize(void)
 {
-	return fdtdec_setup_memory_banksize();
+	int ret;
+
+	ret = fdtdec_setup_memory_banksize();
+	if (ret)
+		return ret;
+
+	gd->bd->bi_dram[0].size = gd->ram_size;
+
+	return 0;
 }
 
 ulong board_get_usable_ram_top(ulong total_size)
