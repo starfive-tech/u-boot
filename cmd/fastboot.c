starfive-tech/u-boot

commit 67df54127e38f1351c38293abd08d817e33394c7

cmd: fastboot: Add presetting on StarFive SoC before fastboot
Add the presetting about usb as device in fastboot function.

Signed-off-by: Xingyu Wu <xingyu.wu@starfivetech.com>

cmd/fastboot.c
@@ -43,6 +43,14 @@ static int do_fastboot_usb(int argc, char *const argv[],
 	char *endp;
 	int ret;
 
+#ifdef CONFIG_FASTBOOT_STARFIVE_MAX_BLK_WRITE
+#define RUN_FB_SF_PRESETTING				\
+	"fdt set /soc/usbdrd starfive,usb2-only <0x1>;"	\
+	"fdt set /soc/usbdrd/usb@10100000 dr_num_mode <0x2>;"
+
+		run_command_list(RUN_FB_SF_PRESETTING, -1, 0);
+#endif
+
 	if (argc < 2)
 		return CMD_RET_USAGE;
 

commit 24a0c6a085b3c17262daaadf0f83d2c3fb11eaf9

cmd: fastboot: Fix failure to enter fastboot
Only USB host and device are same IP and should be changed mode with initialization
before fastboot in Devkits. Add the target of Devkits to math the board.

Fixes: 67df54127e38 ("cmd: fastboot: Add presetting on StarFive SoC before fastboot")
Signed-off-by: Xingyu Wu <xingyu.wu@starfivetech.com>

cmd/fastboot.c
@@ -43,12 +43,12 @@ static int do_fastboot_usb(int argc, char *const argv[],
 	char *endp;
 	int ret;
 
-#ifdef CONFIG_FASTBOOT_STARFIVE_MAX_BLK_WRITE
+#ifdef CONFIG_TARGET_STARFIVE_DEVKITS
 #define RUN_FB_SF_PRESETTING				\
 	"fdt set /soc/usbdrd starfive,usb2-only <0x1>;"	\
 	"fdt set /soc/usbdrd/usb@10100000 dr_num_mode <0x2>;"
 
-		run_command_list(RUN_FB_SF_PRESETTING, -1, 0);
+	run_command_list(RUN_FB_SF_PRESETTING, -1, 0);
 #endif
 
 	if (argc < 2)