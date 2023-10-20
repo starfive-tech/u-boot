50a957d1a11b6f931c33ae1ce8e5972144289566
Wei Liang Lim <weiliang.lim@starfivetech.com>
2023-10-20 22:46:21 +0800

    include: configs: starfive-dubhe-fpga.h: Update serverip for Dubhe
    Signed-off-by: Wei Liang Lim <weiliang.lim@starfivetech.com>

--- include/configs/starfive-dubhe-fpga.h
+++ include/configs/starfive-dubhe-fpga.h
@@ -4,7 +4,7 @@
 #define CFG_EXTRA_ENV_SETTINGS								\
 	"ipaddr=192.168.152.100\0"                          				\
 	"netmask=255.255.255.0\0"                       				\
-	"serverip=192.168.152.30\0"                        				\
+	"serverip=192.168.152.85\0"                        				\
 	"dubhe_boot=booti 0xa1000000 - 0xa0000000;\0"			\
 	"tftp_boot=tftp 0xa1000000 Image; tftp 0xa0000000 dubhe_fpga.dtb; 		\
 		   run dubhe_boot;\0"				\
