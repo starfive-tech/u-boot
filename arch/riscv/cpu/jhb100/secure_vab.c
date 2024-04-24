0b98ca8baa93aa97a35d791d018ab05e6dbab594
weishengchng <weisheng.chng@starfivetech.com>
2024-04-24 14:58:35 +0800

    arch: riscv: cpu: jhb100: secure_vab: [PLACEHOLDER] Add StarFive JHB100 secure VAB driver
    Add StarFive JHB100 secure VAB driver support
    
    Signed-off-by: weishengchng <weisheng.chng@starfivetech.com>

---
+++ arch/riscv/cpu/jhb100/secure_vab.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2021-2023 StarFive Technology Co., Ltd.
+ *
+ */
+
+#include <dm/uclass.h>
+#include <dm/device.h>
+#include <hang.h>
+#include <misc.h>
+#include <asm/io.h>
+#include <asm/sbi.h>
+
+void board_fit_image_post_process(const void *fit, int node, void **p_image,
+				  size_t *p_size)
+{
+
+}
