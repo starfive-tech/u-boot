9c97c8406617b93da27d4d59d2f1749d4f379182
Wei Sheng Chng <weisheng.chng@sw_buildbot@starfivetech.com>
2024-09-11 11:13:27 +0800

    arch: riscv: include: asm: arch-jhb100: Enable boot strap selection for boot source and env
    Enable boot strap mapping for boot source and environment saving
    
    Signed-off-by: Wei Sheng Chng <weisheng.chng@sw_buildbot@starfivetech.com>

--- arch/riscv/include/asm/arch-jhb100/boot_src.h
+++ arch/riscv/include/asm/arch-jhb100/boot_src.h
@@ -7,21 +7,21 @@
 
 #include <asm/io.h>
 
-#define AON_SCRATCHPAD0_BASE_ADDR  0x001F32001C
+#define SYS2_SYSCON_ADDR	0x00130180c8
 
-#define BOOT_SRC_SFC            0x0
-#define BOOT_SRC_EMMC           0x1
-#define BOOT_SRC_UFS            0x2
-#define BOOT_SRC_UART           0x3
-#define BOOT_SRC_GMAC           0x4
-#define BOOT_SRC_AUTO_DETECT    0x5
-/*Debugging*/
-#define HARD_SELECT_BOOT_SRC_RAM   0x6
-#define BOOT_SRC_MASK              GENMASK(1, 0)
+#define BOOT_SRC_SFC		0x0
+#define BOOT_SRC_EMMC		0x1
+#define BOOT_SRC_UFS		0x2
+#define BOOT_SRC_UART		0x3
+#define BOOT_SRC_GMAC		0x4
+#define BOOT_SRC_AUTO_DETECT	0x5
+/* Debugging */
+#define HARD_SELECT_BOOT_SRC_RAM	0x6
+#define BOOT_SRC_MASK			GENMASK(2, 0)
 
-/*Get boot source reg value*/
-#define GET_AON_SCRATCHPAD0(x)     readl((const void *)AON_SCRATCHPAD0_BASE_ADDR + ((x) << 2))
-#define GET_BOOT_SRC               (GET_AON_SCRATCHPAD0(0) & BOOT_SRC_MASK)
+/* Get boot source reg value */
+#define GET_BOOT_STRAP(x)	readl((const void *)SYS2_SYSCON_ADDR + ((x) << 2))
+#define GET_BOOT_SRC		(GET_BOOT_STRAP(0) & BOOT_SRC_MASK)
 
 u32 jhb100_get_boot_device(void);
 
