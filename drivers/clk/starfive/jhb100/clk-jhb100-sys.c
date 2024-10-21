8ec01a7a56b7c5110c8c8ba10a998afcc71f5e14
zhiwei.lim <zhiwei.lim@starfivetech.com>
2024-10-21 09:24:20 +0800

    drivers: clk: starfive: Update clk drivers for phase 3
    Update the following clock drivers:
    - per0crg
    - per1crg
    - per2crg
    - per3crg
    - syscrg
    - sys1crg
    - sys2crg
    
    Signed-off-by: zhiwei.lim <zhiwei.lim@starfivetech.com>

--- drivers/clk/starfive/jhb100/clk-jhb100-sys.c
+++ drivers/clk/starfive/jhb100/clk-jhb100-sys.c
@@ -12,7 +12,7 @@
 
 static const char *apb_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
-	[1] = "apb_main",
+	[1] = "apb_main_sys0",
 };
 
 static const char *bmcpcierp_600_mux[MAX_NUM_PARENTS] = {
@@ -35,6 +35,11 @@ static const char *hostss0_600_mux[MAX_NUM_PARENTS] = {
 	[1] = "hostss0_600",
 };
 
+static const char *hostss0_espi0_mux[MAX_NUM_PARENTS] = {
+	[0] = "sys_espi0",
+	[1] = "gpio_espi0_66",
+};
+
 static const char *bmcusb_600_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
 	[1] = "bmcusb_600",
@@ -43,6 +48,11 @@ static const char *bmcusb_600_mux[MAX_NUM_PARENTS] = {
 static const char *bmcusb_200_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
 	[1] = "bmcusb_200",
+};
+
+static const char *bmccpuss_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "pll1",
 };
 
 static const char *vce_600_mux[MAX_NUM_PARENTS] = {
@@ -75,90 +85,245 @@ static const char *bmcper2_125_mux[MAX_NUM_PARENTS] = {
 	[1] = "per2_125",
 };
 
+static const char *hostss1_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "hostss1_600",
+};
+
+static const char *vout_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "vout_600",
+};
+
+static const char *bmcper3_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "per3_600",
+};
+
+static const char *hostusb_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "hostusb_600",
+};
+
+static const char *hostusbcmn_480_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "hostusbcmn_480",
+};
+
+static const char *bmcper1_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "per1_600",
+};
+
+static const char *bmcper1_400_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "per1_400",
+};
+
+static const char *bmcper1_800_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "per1_800",
+};
+
+static const char *bmcper0_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "bmcper0_600",
+};
+
+static const char *bmcper0_400_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "per0_400",
+};
+
+static const char *bmcper0_800_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "bmcper0_800",
+};
+
+static const char *gpu0_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "gpu0_600",
+};
+
+static const char *gpu1_600_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "gpu1_600",
+};
+
 static struct clk_info syscrg_clk_info[] = {
 	/* root */
-	{ JHB100_SYSCLK_APB_MAIN,	"apb_main",
-		"pll1",		NULL,	CLK_DIVIDER,	4 },
-	{ JHB100_SYSCLK_APB_OSC,	"apb_osc",
-		"",	apb_mux,	CLK_MUX,	0 },
-	/* top noc */
+	{ JHB100_SYSCLK_APB_MAIN_SYS0,	"apb_main_sys0",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_APB_OSC_SYS0,	"apb_osc_sys0",
+		"",	apb_mux,		CLK_MUX,	0 },
+	/* top noc 0 */
 	{ JHB100_SYSCLK_NCNOC0_600,	"ncnoc0_600",
-		"pll0",		NULL,	CLK_DIVIDER,	4 },
-	{ JHB100_SYSCLK_NCNOC0_SYSTOP_CLK_NCNOC_DATA2,	"ncnoc0_systop_clk_ncnoc_data2",
-		"ncnoc0_600",	NULL,	CLK_INV,	0 },
-	/* debug */
-	{ JHB100_SYSCLK_DEBUG_200,	"debug_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	4 },
 	/* sim apb */
-	{ JHB100_SYSCLK_APB_SIM_APB,	"apb_sim_apb",
-		"osc",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_APB_SIM_APB_ICG_BUF,	"apb_sim_apb_icg_buf",
+		"apb_osc_sys0",		NULL,	CLK_GATE,	0 },
 	/* bmcpcierp */
 	{ JHB100_SYSCLK_BMCPCIERP_600,	"bmcpcierp_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_BMCPCIERP_MAIN,	"bmcpcierp_main",
 		"",	bmcpcierp_600_mux,	CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCPCIERP_100,	"bmcpcierp_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_BMCPCIERP_CFG,	"bmcpcierp_cfg",
 		"",	bmcpcierp_100_mux,	CLK_MUX,	0 },
-	{ JHB100_SYSCLK_BMCPCIERP_PHY_SCAN_200,	"bmcpcierp_phy_scan_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYSCLK_BMCPCIERP_PHY_SCAN_500,	"bmcpcierp_phy_scan_500",
-		"pll1",		NULL,	CLK_GDIV,	2 },
-	{ JHB100_SYSCLK_BMCPCIERP_REF_CML,	"bmcpcierp_ref_cml",
-		"pll1",		NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_BMCPCIERP_PHY_SCAN_400_DFT,	"bmcpcierp_phy_scan_400_dft",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_PCIE_REF_CML,	"pcie_ref_cml",
+		"pll0",			NULL,	CLK_DIVIDER,	5 },
+	{ JHB100_SYSCLK_BMCPCIERP_OSC,	"bmcpcierp_osc",
+		"osc",			NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_NCNOC_DATA_INIT,	"bmcpcierp_ncnoc_data_init",
+		"bmcpcierp_main",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_NCNOC_CFG_INIT,	"bmcpcierp_ncnoc_cfg_init",
+		"bmcpcierp_cfg",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_NCNOC_TARG,	"bmcpcierp_ncnoc_targ",
+		"bmcpcierp_main",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_PCIE_PHY_SCAN_1000_DFT,	"bmcpcierp_pcie_phy_scan_1000_dft",
+		"pll1",			NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_PCIE_PHY_SCAN_400_DFT,	"bmcpcierp_pcie_phy_scan_400_dft",
+		"bmcpcierp_phy_scan_400_dft",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_BMCPCIERP_PCU,	"bmcpcierp_pcu",
+		"osc",			NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_U0_PCIE_REF_CML_CMOSIN,	"u0_pcie_ref_cml_cmosin",
+		"pcie_ref_cml",		NULL,	CLK_GATE,	0 },
 	/* hostss0 */
 	{ JHB100_SYSCLK_HOSTSS0_100,	"hostss0_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_HOSTSS0_CFG,	"hostss0_cfg",
 		"",	hostss0_100_mux,	CLK_MUX,	0 },
 	{ JHB100_SYSCLK_HOSTSS0_600,	"hostss0_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_HOSTSS0_DATA,	"hostss0_data",
 		"",	hostss0_600_mux,	CLK_MUX,	0 },
-	{ JHB100_SYSCLK_HOSTSS0_PHY_SCAN_200,	"hostss0_phy_scan_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYSCLK_HOSTSS0_PHY_SCAN_500,	"hostss0_phy_scan_500",
-		"pll1",		NULL,	CLK_GDIV,	2 },
-	{ JHB100_SYSCLK_HOSTSS0_REF_CML,	"hostss0_ref_cml",
-		"pll1",		NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_HOSTSS0_PHY_SCAN_400,	"hostss0_phy_scan_400",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_GPIO_ESPI0_66,	"gpio_espi0_66",
+		"pll2",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_ESPI0_EXT_DFT_MUX,	"espi0_ext_dft_mux",
+		"",	hostss0_espi0_mux,	CLK_MUX,	0 },
+	{ JHB100_SYSCLK_HOSTSS0_PCIE_PHY_SCAN_400_DFT,	"hostss0_pcie_phy_scan_400_dft",
+		"hostss0_phy_scan_400",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYSCLK_HOSTSS0_PCIE_PHY_SCAN_1000_DFT,	"hostss0_pcie_phy_scan_1000_dft",
+		"pll1",			NULL,	CLK_GATE,	0 },
 	/* bmcusb */
 	{ JHB100_SYSCLK_BMCUSB_600,	"bmcusb_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_BMCUSB_INIT,	"bmcusb_init",
-		"",	bmcusb_600_mux,	CLK_MUX,	0 },
+		"",	bmcusb_600_mux,		CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCUSB_200,	"bmcusb_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_BMCUSB_TARG,	"bmcusb_targ",
-		"",	bmcusb_200_mux,	CLK_MUX,	0 },
+		"",	bmcusb_200_mux,		CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCUSB_SCANCLK,	"bmcusb_scanclk",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCUSB_480M_SCANCLK,	"bmcusb_480m_scanclk",
+		"bmcusb_scanclk",	NULL,	CLK_GATE,	0 },
+	/* bmccpuss */
+	{ JHB100_SYSCLK_BMCCPUSS_SYS_OSC,	"bmccpuss_sys_osc",
+		"",	bmccpuss_mux,		CLK_GMUX,	0 },
 	/* vce */
 	{ JHB100_SYSCLK_VCE_600,	"vce_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_VCE_INIT,	"vce_init",
-		"",	vce_600_mux,	CLK_MUX,	0 },
+		"",	vce_600_mux,		CLK_MUX,	0 },
 	{ JHB100_SYSCLK_VCE_100,	"vce_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_VCE_TARG,	"vce_targ",
-		"",	vce_100_mux,	CLK_MUX,	0 },
+		"",	vce_100_mux,		CLK_MUX,	0 },
 	/* bmcperiph2 */
 	{ JHB100_SYSCLK_BMCPER2_600,	"bmcper2_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYSCLK_BMCPER2_INIT,	"bmcper2_init",
 		"",	bmcper2_600_mux,	CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCPER2_100,	"bmcper2_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_BMCPER2_TARG,	"per2_targ",
 		"",	bmcper2_100_mux,	CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCPER2_400,	"per2_400",
-		"pll0",		NULL,	CLK_GDIV,	4 },
+		"pll0",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_BMCPER2_400_OSC,	"bmcper2_400_osc",
 		"",	bmcper2_400_mux,	CLK_MUX,	0 },
 	{ JHB100_SYSCLK_BMCPER2_125,	"per2_125",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYSCLK_BMCPER2_125_OSC,	"bmcper2_125_osc",
 		"",	bmcper2_125_mux,	CLK_MUX,	0 },
+	/* top noc 1*/
+	{ JHB100_SYSCLK_NCNOC1_600,	"ncnoc1_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	/* hostss1 */
+	{ JHB100_SYSCLK_HOSTSS1_600,	"hostss1_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_HOSTSS1_DATA,	"hostss1_data",
+		"",	hostss1_mux,		CLK_MUX,	0 },
+	{ JHB100_SYSCLK_HOSTSS1_PHY_SCAN_400,	"hostss1_phy_scan_400",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_HOSTSS1_PHY_SCAN_400_ICG_BUF,	"hostss1_phy_scan_400_icg_buf",
+		"hostss1_phy_scan_400",	NULL,	CLK_GATE,	0 },
+	/* vout */
+	{ JHB100_SYSCLK_VOUT_600,	"vout_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_VOUT_INIT,	"vout_init",
+		"",	vout_600_mux,		CLK_MUX,	0 },
+	{ JHB100_SYSCLK_VOUT_AUX,	"vout_aux",
+		"pll0",			NULL,	CLK_DIVIDER,	8 },
+	{ JHB100_SYSCLK_VOUT_SCAN_800_DFT,	"vout_scan_800_dft",
+		"pll0",			NULL,	CLK_DIVIDER,	2 },
+	/* bmcperiph3 */
+	{ JHB100_SYSCLK_BMCPER3_600,	"per3_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCPER3_INIT,	"bmcper3_init",
+		"",	bmcper3_600_mux,	CLK_MUX,	0 },
+	/* hostusb */
+	{ JHB100_SYSCLK_HOSTUSB_600,	"hostusb_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_HOSTUSB_INIT,	"hostusb_init",
+		"",	hostusb_600_mux,	CLK_MUX,	0 },
+	/* hostusbcmn */
+	{ JHB100_SYSCLK_HOSTUSBCMN_480,	"hostusbcmn_480",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_HOSTUSBCMN_480_OSC,	"hostusbcmn_480_osc",
+		"",	hostusbcmn_480_mux,	CLK_MUX,	0 },
+	/* bmcperiph1 */
+	{ JHB100_SYSCLK_BMCPER1_600,	"per1_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCPER1_INIT,	"per1_init",
+		"",	bmcper1_600_mux,	CLK_MUX,	0 },
+	{ JHB100_SYSCLK_BMCPER1_400,	"per1_400",
+		"pll0",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_BMCPER1_400_OSC,	"bmcper1_400_osc",
+		"",	bmcper1_400_mux,	CLK_MUX,	0 },
+	{ JHB100_SYSCLK_BMCPER1_800,	"per1_800",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCPER1_800_OSC,	"bmcper1_800_osc",
+		"",	bmcper1_800_mux,	CLK_MUX,	0 },
+	/* bmcperiph0 */
+	{ JHB100_SYSCLK_BMCPER0_600,	"bmcper0_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCPER0_INIT,	"per0_init",
+		"",	bmcper0_600_mux,	CLK_MUX,	0 },
+	{ JHB100_SYSCLK_BMCPER0_400,	"per0_400",
+		"pll0",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYSCLK_BMCPER0_400_OSC,	"bmcper0_400_osc",
+		"",	bmcper0_400_mux,	CLK_MUX,	0 },
+	{ JHB100_SYSCLK_BMCPER0_800,	"bmcper0_800",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_BMCPER1_800_OSC,	"per0_800_osc",
+		"",	bmcper0_800_mux,	CLK_MUX,	0 },
+	/* gpu0 */
+	{ JHB100_SYSCLK_GPU0_600,	"gpu0_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_GPU0_INIT,	"sys2_gpu0_init",
+		"",	gpu0_600_mux,		CLK_MUX,	0 },
+	/* gpu1 */
+	{ JHB100_SYSCLK_GPU1_600,	"gpu1_600",
+		"pll0",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYSCLK_GPU1_INIT,	"sys2_gpu1_init",
+		"",	gpu1_600_mux,		CLK_MUX,	0 },
 };
 
 static int jhb100_syscrg_probe(struct udevice *dev)
--- drivers/clk/starfive/jhb100/clk-jhb100-sys2.c
+++ drivers/clk/starfive/jhb100/clk-jhb100-sys2.c
@@ -15,16 +15,6 @@ static const char *apb_mux[MAX_NUM_PARENTS] = {
 	[1] = "apb_main_sys2",
 };
 
-static const char *hostusb_600_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "hostusb_600",
-};
-
-static const char *hostusb_200_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "hostusb_200",
-};
-
 static const char *hostusb_100_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
 	[1] = "hostusb_100",
@@ -35,24 +25,9 @@ static const char *hostusbcmn_500_mux[MAX_NUM_PARENTS] = {
 	[1] = "hostusbcmn_500",
 };
 
-static const char *hostusbcmn_480_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "hostusbcmn_480",
-};
-
-static const char *bmcper1_600_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "bmcper1_600",
-};
-
 static const char *bmcper1_200_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
-	[1] = "bmcper1_200",
-};
-
-static const char *bmcper1_400_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "per1_400",
+	[1] = "per1_200",
 };
 
 static const char *bmcper1_250_mux[MAX_NUM_PARENTS] = {
@@ -60,24 +35,9 @@ static const char *bmcper1_250_mux[MAX_NUM_PARENTS] = {
 	[1] = "bmcper1_250",
 };
 
-static const char *bmcper1_800_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "per1_800",
-};
-
-static const char *bmcper0_600_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "bmcper0_600",
-};
-
 static const char *bmcper0_200_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
 	[1] = "bmcper0_200",
-};
-
-static const char *bmcper0_400_mux[MAX_NUM_PARENTS] = {
-	[0] = "osc",
-	[1] = "per0_400",
 };
 
 static const char *bmcper0_125_mux[MAX_NUM_PARENTS] = {
@@ -85,87 +45,109 @@ static const char *bmcper0_125_mux[MAX_NUM_PARENTS] = {
 	[1] = "per0_125",
 };
 
-static const char *pll6_mux[MAX_NUM_PARENTS] = {
+static const char *gpu0_100_mux[MAX_NUM_PARENTS] = {
 	[0] = "osc",
-	[1] = "pll6",
+	[1] = "gpu0_100",
+};
+
+static const char *gpu1_100_mux[MAX_NUM_PARENTS] = {
+	[0] = "osc",
+	[1] = "gpu1_100",
 };
 
 static struct clk_info sys2crg_clk_info[] = {
 	/* root */
 	{ JHB100_SYS2CLK_APB_MAIN_SYS2,	"apb_main_sys2",
-		"pll1",		NULL,	CLK_DIVIDER,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYS2CLK_APB_OSC_SYS2,	"apb_osc_sys2",
-		"",	apb_mux,	CLK_MUX,	0 },
+		"",	apb_mux,		CLK_MUX,	0 },
 	/* jtag mst*/
-	{ JHB100_SYS2CLK_JTAGM_200,	"jtagm_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_JTAGM_100,	"jtagm_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+	{ JHB100_SYS2CLK_JTAGM0_200,	"jtagm0_200",
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYS2CLK_JTAGM1_200,	"jtagm1_200",
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
+	{ JHB100_SYS2CLK_JTAGM0_100,	"jtagm0_100",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYS2CLK_JTAGM1_100,	"jtagm1_100",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYS2CLK_JTAGM0_ATPG_TCLOCK,	"jtagm0_atpg_tclock",
+		"jtagm0_100",		NULL,	CLK_DIVIDER,	2 },
+	{ JHB100_SYS2CLK_JTAGM1_ATPG_TCLOCK,	"jtagm1_atpg_tclock",
+		"jtagm1_100",		NULL,	CLK_DIVIDER,	2 },
+	{ JHB100_SYS2CLK_JTAG0_MST_WRAP_HCLK,	"jtag0_mst_wrap_hclk",
+		"jtagm0_200",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG0_MST_WRAP_CLK_JTAG,	"jtag0_mst_wrap_clk_jtag",
+		"jtagm0_200",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG0_MST_WRAP_APB_PCLK,	"jtag0_mst_wrap_apb_pclk",
+		"jtagm0_100",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG0_MST_WRAP_ATPG_TCLOCK,	"jtag0_mst_wrap_atpg_tclock",
+		"jtagm0_100",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG1_MST_WRAP_HCLK,	"jtag1_mst_wrap_hclk",
+		"jtagm1_200",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG1_MST_WRAP_CLK_JTAG,	"jtag1_mst_wrap_clk_jtag",
+		"jtagm1_200",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG1_MST_WRAP_APB_PCLK,	"jtag1_mst_wrap_apb_pclk",
+		"jtagm1_100",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_JTAG1_MST_WRAP_ATPG_TCLOCK,	"jtag1_mst_wrap_atpg_tclock",
+		"jtagm1_100",		NULL,	CLK_GATE,	0 },
 	/* hostusbcmn */
-	{ JHB100_SYS2CLK_HOSTUSB_600,	"hostusb_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_HOSTUSB_INIT,	"hostusb_init",
-		"",	hostusb_600_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_HOSTUSB_200,	"hostusb_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_HOSTUSB_TARG,	"hostusb_targ",
-		"",	hostusb_200_mux,	CLK_MUX,	0 },
 	{ JHB100_SYS2CLK_HOSTUSB_100,	"hostusb_100",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYS2CLK_HOSTUSBCMN_TARG,	"hostusbcmn_targ",
 		"",	hostusb_100_mux,	CLK_MUX,	0 },
 	{ JHB100_SYS2CLK_HOSTUSBCMN_500,	"hostusbcmn_500",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYS2CLK_HOSTUSBCMN_500_OSC,	"hostusbcmn_500_osc",
 		"",	hostusbcmn_500_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_HOSTUSBCMN_480,	"hostusbcmn_480",
-		"pll0",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_HOSTUSBCMN_480_OSC,	"hostusbcmn_480_osc",
-		"",	hostusbcmn_480_mux,	CLK_MUX,	0 },
 	/* bmcperiph1 */
-	{ JHB100_SYS2CLK_BMCPER1_600,	"bmcper1_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_BMCPER1_INIT,	"per1_init",
-		"",	bmcper1_600_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_BMCPER1_200,	"bmcper1_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+	{ JHB100_SYS2CLK_BMCPER1_200,	"per1_200",
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYS2CLK_BMCPER1_TARG,	"per1_targ",
 		"",	bmcper1_200_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_BMCPER1_400,	"per1_400",
-		"pll0",		NULL,	CLK_GDIV,	4 },
-	{ JHB100_SYS2CLK_BMCPER1_400_OSC,	"bmcper1_400_osc",
-		"",	bmcper1_400_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_UFS_REF,	"ufs_ref",
-		"pll7",		NULL,	CLK_GDIV,	5 },
 	{ JHB100_SYS2CLK_BMCPER1_250,	"bmcper1_250",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYS2CLK_BMCPER1_250_OSC,	"bmcper1_250_osc",
 		"",	bmcper1_250_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_BMCPER1_143,	"bmcper1_143",
-		"pll1",		NULL,	CLK_GDIV,	4 },
-	{ JHB100_SYS2CLK_BMCPER1_800,	"per1_800",
-		"pll0",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_BMCPER1_800_OSC,	"bmcper1_800_osc",
-		"",	bmcper1_800_mux,	CLK_MUX,	0 },
+	{ JHB100_SYS2CLK_BMCPER1_143_DFT,	"bmcper1_143_dft",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYS2CLK_BMCPER1_143,	"per1_143",
+		"bmcper1_143_dft",	NULL,	CLK_GATE,	0 },
 	/* bmcperiph0 */
-	{ JHB100_SYS2CLK_BMCPER0_600,	"bmcper0_600",
-		"pll0",		NULL,	CLK_GDIV,	3 },
-	{ JHB100_SYS2CLK_BMCPER0_INIT,	"per0_init",
-		"",	bmcper0_600_mux,	CLK_MUX,	0 },
 	{ JHB100_SYS2CLK_BMCPER0_200,	"bmcper0_200",
-		"pll1",		NULL,	CLK_GDIV,	3 },
+		"pll1",			NULL,	CLK_DIVIDER,	3 },
 	{ JHB100_SYS2CLK_BMCPER0_TARG,	"per0_targ",
 		"",	bmcper0_200_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_BMCPER0_400,	"per0_400",
-		"pll0",		NULL,	CLK_GDIV,	4 },
-	{ JHB100_SYS2CLK_BMCPER0_400_OSC,	"bmcper0_400_osc",
-		"",	bmcper0_400_mux,	CLK_MUX,	0 },
 	{ JHB100_SYS2CLK_BMCPER0_125,	"per0_125",
-		"pll1",		NULL,	CLK_GDIV,	4 },
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
 	{ JHB100_SYS2CLK_BMCPER0_125_OSC,	"bmcper0_125_osc",
 		"",	bmcper0_125_mux,	CLK_MUX,	0 },
-	{ JHB100_SYS2CLK_BMCPER0_PLL6,		"bmcper0_pll6",
-		"",	pll6_mux,	CLK_GMUX,	0 },
+	/* gpu0 */
+	{ JHB100_SYS2CLK_GPU0_100,	"gpu0_100",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYS2CLK_GPU0_TARG,	"gpu0_targ",
+		"",	gpu0_100_mux,		CLK_MUX,	0 },
+	{ JHB100_SYS2CLK_GPU0_BUS_CLK,	"gpu0_bus_clk",
+		"sys2_gpu0_init",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_GPU0_APB_CLK,	"gpu0_apb_clk",
+		"gpu0_targ",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_GPU0_OSC_CLK,	"gpu0_osc_clk",
+		"osc",			NULL,	CLK_GATE,	0 },
+	/* gpu1 */
+	{ JHB100_SYS2CLK_GPU1_100,	"gpu1_100",
+		"pll1",			NULL,	CLK_DIVIDER,	4 },
+	{ JHB100_SYS2CLK_GPU1_TARG,	"gpu1_targ",
+		"",	gpu1_100_mux,		CLK_MUX,	0 },
+	{ JHB100_SYS2CLK_GPU1_BUS_CLK,	"gpu1_bus_clk",
+		"sys2_gpu1_init",	NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_GPU1_APB_CLK,	"gpu1_apb_clk",
+		"gpu1_targ",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_GPU1_OSC_CLK,	"gpu1_osc_clk",
+		"osc",			NULL,	CLK_GATE,	0 },
+	/* main icg */
+	{ JHB100_SYS2CLK_MAIN_ICG_EN_JTAG0,	"main_icg_en_jtag0",
+		"jtagm0_200",		NULL,	CLK_GATE,	0 },
+	{ JHB100_SYS2CLK_MAIN_ICG_EN_JTAG1,	"main_icg_en_jtag1",
+		"jtagm1_200",		NULL,	CLK_GATE,	0 },
 };
 
 static int jhb100_sys2crg_probe(struct udevice *dev)
