21761857c1c12e877676dd05c5b5124f1759c9e6
WeiSheng.Chng <weisheng.chng@starfivetech.com>
2024-11-20 22:02:47 +0800

    arch: riscv: asm: arch-jhb100: boot_src.h: Update boot strap address
    Update boot strap address for boot device selection and flash mode
    
    Signed-off-by: WeiSheng.Chng <weisheng.chng@starfivetech.com>

--- arch/riscv/include/asm/arch-jhb100/boot_src.h
+++ arch/riscv/include/asm/arch-jhb100/boot_src.h
@@ -7,21 +7,27 @@
 
 #include <asm/io.h>
 
-#define SYS2_SYSCON_ADDR	0x00130180c8
+#define BOOT_STRAP_ADDR		0x001301A050
 
-#define BOOT_SRC_SFC		0x0
+#define BOOT_SRC_UART		0x0
 #define BOOT_SRC_EMMC		0x1
 #define BOOT_SRC_UFS		0x2
-#define BOOT_SRC_UART		0x3
+#define BOOT_SRC_SFC		0x3
 #define BOOT_SRC_GMAC		0x4
 #define BOOT_SRC_AUTO_DETECT	0x5
 /* Debugging */
 #define HARD_SELECT_BOOT_SRC_RAM	0x6
-#define BOOT_SRC_MASK			GENMASK(2, 0)
+#define BOOT_SRC_MASK			GENMASK(1, 0)
+#define BMC_FLASH_MODE_MASK		GENMASK(0, 0)
+
+#define BOOT_SRC_SHIFT			0x6
+#define BMC_FLASH_MODE_SHIFT		0x1a
 
 /* Get boot source reg value */
-#define GET_BOOT_STRAP(x)	readl((const void *)SYS2_SYSCON_ADDR + ((x) << 2))
-#define GET_BOOT_SRC		(GET_BOOT_STRAP(0) & BOOT_SRC_MASK)
+#define GET_BOOT_STRAP		readl((const void *)BOOT_STRAP_ADDR)
+#define GET_BOOT_SRC		((GET_BOOT_STRAP >> BOOT_SRC_SHIFT) & BOOT_SRC_MASK)
+#define GET_BMC_FLASH_MDOE	((GET_BOOT_STRAP >> BMC_FLASH_MODE_SHIFT) & \
+				BMC_FLASH_MODE_MASK)
 
 u32 jhb100_get_boot_device(void);
 
