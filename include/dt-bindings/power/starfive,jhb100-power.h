2a57e13743c8603f232c97240deae52a8d02eb0f
zhiwei.lim <zhiwei.lim@starfivetech.com>
2024-11-22 18:03:28 +0800

    arch: riscv: dts: jhb100: Sync device tree
    Ensure the device tree is properly synchronized,
    to enable smooth driver operation for phase 4.
    
    Signed-off-by: zhiwei.lim <zhiwei.lim@starfivetech.com>

---
+++ include/dt-bindings/power/starfive,jhb100-power.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 OR MIT */
+/*
+ * Copyright (C) 2024 StarFive Technology Co., Ltd.
+ * Author: Jee Heng Sia <jeeheng.sia@starfivetech.com>
+ *
+ */
+
+#ifndef __DT_BINDINGS_POWER_STARFIVE_JHB100_H__
+#define __DT_BINDINGS_POWER_STARFIVE_JHB100_H__
+
+#define PD_GPU0		0
+#define PD_GPU1		1
+#define PD_DC0		2
+#define PD_DC1		3
+#define PD_HOST0_USB	4
+#define PD_HOST1_USB	5
+#define PD_BMCPCIe_RP	6
+#define PD_HOSTSS_i0	7
+#define PD_HOSTSS_i1	8
+#define PD_UFS		9
+#define PD_NPU		10
+
+#endif /* __DT_BINDINGS_POWER_STARFIVE_JHB100_H__ */
