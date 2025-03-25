c05a4ded982b526df8010f8922b0007e650f616c
zhiwei.lim <zhiwei.lim@starfivetech.com>
2025-03-25 09:47:08 +0800

    arch: riscv: dts: jhb100: Sync device tree
    Ensure the device tree is properly synchronized,
    to enable smooth driver operation.
    
    Signed-off-by: zhiwei.lim <zhiwei.lim@starfivetech.com>

---
+++ include/dt-bindings/gpio/starfive,jhb100-gpio.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2025 StarFive Technology Co., Ltd.
+ * Author: Joshua Yeong <joshua.yeong@starfivetech.com>
+ *
+ */
+
+#ifndef __DT_BINDINGS_GPIO_STARFIVE_JHB100_H__
+#define __DT_BINDINGS_GPIO_STARFIVE_JHB100_H__
+
+#define GPIO_LEVEL_BIT		0x1000000
+
+#define GPIO_NORMAL		0
+#define GPIO_LOW_VOLTAGE	1
+
+#endif /* __DT_BINDINGS_GPIO_STARFIVE_JHB100_H__ */
