f99a3bd2d9519a37cbe170b0d9f844989a9c41d3
zhiwei.lim <zhiwei.lim@starfivetech.com>
2025-03-29 14:12:33 +0800

    board: starfive: jhb100: Remove unnecessary pin register setting
    Since pinmux settings are performed by device tree in SPL, the
    current write register action can be removed.
    
    Signed-off-by: zhiwei.lim <zhiwei.lim@starfivetech.com>

--- board/starfive/jhb100/spl.c
+++ board/starfive/jhb100/spl.c
@@ -31,9 +31,6 @@
 #include <asm/arch/starfive_pcu.h>
 #include <asm/arch/saif_init.h>
 
-#define OUTPUT_ENABLE   0
-#define INPUT_ENABLE    1
-
 /* I2C filter */
 #define JHB100_I2C0_FILTER_ADDR		0x14080000UL
 #define JHB100_I2C_FILTER_OFFSET	0x1000
@@ -43,25 +40,6 @@
 #define JHB100_CPUSS_SECURE_CRG_ADDR		0x14142000UL
 #define JHB100_MAIN_ICG_EN_INT_CTRL_OFFSET	0x60
 #define JHB100_MAIN_CLK_ENABLE			BIT(31)
-
-/* SYS2_IOMUX */
-#define JHB100_SYS2_IOMUX_ADDR			0x13082000UL
-#define JHB100_SYS2_GPIOW_VSEL_OFFSET		0x000
-#define JHB100_SYS2_GPIOW_VSEL_INNER_OFFSET	0x004
-#define JHB100_SYS2_FUNC_SEL_OFFSET		0x0d4
-#define JHB100_SYS2_PADCFG_A36_OFFSET		0x040
-#define JHB100_SYS2_PADCFG_A37_OFFSET		0x044
-#define JHB100_SYS2_FUNC12_SEL			BIT(24)
-#define JHB100_SYS2_FUNC13_SEL			BIT(26)
-
-/* PER2_IOMUX */
-#define JHB100_PER2_IOMUX_ADDR			0x11bc2000UL
-#define JHB100_PER2_GPIONW_VSEL_OFFSET		0x000
-
-#define JHB100_PINCONF_IE		BIT(2)
-#define JHB100_PINCONF_DS_12mA		BIT(0) | BIT(1)
-#define JHB100_VREF_3_3V		0
-#define JHB100_VREF_1_8V		2
 
 int spl_board_init_f(void)
 {
@@ -121,30 +99,6 @@ void jhb100_plat_init(void)
 	 */
 	addr = (void *)(JHB100_CPUSS_SECURE_CRG_ADDR + JHB100_MAIN_ICG_EN_INT_CTRL_OFFSET);
 	writel(JHB100_MAIN_CLK_ENABLE, addr);
-
-	/* SPL has limited DT parsing and does not automatically initialize pinmux settings.
-	 * Use writel() directly to set UART5 pins in SPL.
-	 */
-	addr = (void *)(JHB100_SYS2_IOMUX_ADDR + JHB100_SYS2_GPIOW_VSEL_OFFSET);
-	writel(readl(addr) | JHB100_VREF_3_3V, addr);
-
-	addr = (void *)(JHB100_SYS2_IOMUX_ADDR + JHB100_SYS2_GPIOW_VSEL_INNER_OFFSET);
-	writel(readl(addr) | JHB100_VREF_3_3V, addr);
-
-	addr = (void *)(JHB100_SYS2_IOMUX_ADDR + JHB100_SYS2_FUNC_SEL_OFFSET);
-	writel(readl(addr) | JHB100_SYS2_FUNC12_SEL | JHB100_SYS2_FUNC13_SEL, addr);
-
-	addr = (void *)(JHB100_SYS2_IOMUX_ADDR + JHB100_SYS2_PADCFG_A36_OFFSET);
-	writel(JHB100_PINCONF_DS_12mA, addr);
-
-	addr = (void *)(JHB100_SYS2_IOMUX_ADDR + JHB100_SYS2_PADCFG_A37_OFFSET);
-	writel(JHB100_PINCONF_DS_12mA | JHB100_PINCONF_IE, addr);
-
-	/* SPL has limited DT parsing and does not automatically initialize pinmux settings.
-	 * Use writel() directly to set GMAC2 pins in SPL.
-	 */
-	addr = (void *)(JHB100_PER2_IOMUX_ADDR + JHB100_PER2_GPIONW_VSEL_OFFSET);
-	writel(readl(addr) | JHB100_VREF_1_8V, addr);
 }
 
 void plat_gmac_init(void)
