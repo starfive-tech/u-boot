52fb4339ab18f3575961685136c75a5a084d704e
WeiSheng.Chng <weisheng.chng@starfivetech.com>
2025-05-22 21:15:54 +0800

    arch: riscv: cpu: jhb100: boot_recovery.c: Uboot CLI command for supporting kernel fallback in storages
    To be part of critical boot command for kernel fallback flow
    
    Signed-off-by: WeiSheng.Chng <weisheng.chng@starfivetech.com>

--- arch/riscv/cpu/jhb100/boot_recovery.c
+++ arch/riscv/cpu/jhb100/boot_recovery.c
@@ -4,10 +4,12 @@
  */
 
 #include <asm/arch/boot_mapping.h>
+#include <asm/arch/boot_pti.h>
 #include <asm/arch/boot_src.h>
 #include <bootm.h>
 #include <command.h>
 #include <env.h>
+#include <env_internal.h>
 #include <linux/bitops.h>
 #include <mapmem.h>
 #include <dm.h>
@@ -103,8 +105,8 @@ static int do_starfive_print_rec_map(struct cmd_tbl *cmdtp, int flag, int argc,
 	return CMD_RET_SUCCESS;
 }
 
-static int do_starfive_authenticate(struct cmd_tbl *cmdtp, int flag, int argc,
-				    char *const argv[])
+static int do_starfive_authenticate_fit_mem(struct cmd_tbl *cmdtp, int flag, int argc,
+					    char *const argv[])
 {
 	struct bootm_info bm_info;
 	int ret;
@@ -133,6 +135,114 @@ static int do_starfive_authenticate(struct cmd_tbl *cmdtp, int flag, int argc,
 	return ret ? CMD_RET_FAILURE : CMD_RET_SUCCESS;
 }
 
+static int do_starfive_authenticate_storage(struct cmd_tbl *cmdtp, int flag, int argc,
+					    char *const argv[])
+{
+	int ret;
+
+	argc--; argv++;
+	if (argc) {
+		switch (hextoul(argv[0], NULL)) {
+		case 1:
+			ret = starfive_req_img_auth_storage(BOOT_SRC_EMMC,
+							    PT_ACTIVE,
+							    IMG_TYPE_KERNEL);
+			break;
+		case 2:
+			ret = starfive_req_img_auth_storage(BOOT_SRC_EMMC,
+							    PT_GOLDEN,
+							    IMG_TYPE_KERNEL);
+			break;
+		case 3:
+			ret = starfive_req_img_auth_memory(BOOT_SRC_SFC,
+							   PT_ACTIVE,
+							   IMG_TYPE_KERNEL);
+			break;
+		case 4:
+			ret = starfive_req_img_auth_memory(BOOT_SRC_SFC,
+							   PT_GOLDEN,
+							   IMG_TYPE_KERNEL);
+			break;
+		default:
+			printf("Unknown argument, refer to help command...\n");
+		}
+	}
+
+	if (ret)
+		return CMD_RET_FAILURE;
+	return CMD_RET_SUCCESS;
+}
+
+static int do_starfive_pre_os_boot_notify(struct cmd_tbl *cmdtp, int flag, int argc,
+					  char *const argv[])
+{
+	starfive_pre_os_boot_notify();
+	return CMD_RET_SUCCESS;
+}
+
+static int do_starfive_check_sfc_dual_flash(struct cmd_tbl *cmdtp, int flag, int argc,
+					    char *const argv[])
+{
+	if (starfive_get_sfc_cs_line_num() < 2) {
+		printf("Only single SPI flash chip detected\n");
+		return CMD_RET_FAILURE;
+	}
+	printf("Dual SPI flash chip detected\n");
+	return CMD_RET_SUCCESS;
+}
+
+static int do_starfive_get_img_info(struct cmd_tbl *cmdtp, int flag, int argc,
+				    char *const argv[])
+{
+	u32 val;
+
+	argc--; argv++;
+	if (argc) {
+		switch (hextoul(argv[0], NULL)) {
+		case 1:
+			val = starfive_get_partition_num(BOOT_SRC_EMMC,
+							 PT_ACTIVE,
+							 IMG_TYPE_KERNEL);
+			env_set_hex("emmc_kernel_act_part_num", (ulong)val);
+			val = starfive_get_partition_offset(BOOT_SRC_EMMC,
+							    PT_ACTIVE,
+							    IMG_TYPE_KERNEL);
+			env_set_hex("emmc_kernel_act_part_offs", (ulong)val);
+			break;
+		case 2:
+			val = starfive_get_partition_num(BOOT_SRC_EMMC,
+							 PT_GOLDEN,
+							 IMG_TYPE_KERNEL);
+			env_set_hex("emmc_kernel_gol_part_num", (ulong)val);
+			val = starfive_get_partition_offset(BOOT_SRC_EMMC,
+							    PT_GOLDEN,
+							    IMG_TYPE_KERNEL);
+			env_set_hex("emmc_kernel_gol_part_offs", (ulong)val);
+			break;
+		case 3:
+			val = starfive_get_partition_offset(BOOT_SRC_SFC,
+							    PT_ACTIVE,
+							    IMG_TYPE_KERNEL);
+			env_set_hex("sfc_kernel_act_part_offs", (ulong)val);
+			break;
+		case 4:
+			if (starfive_get_sfc_cs_line_num() < 2) {
+				printf("SFC Golden image not found...\n");
+				printf("Golden image is stored in second flash chip...\n");
+			} else {
+				val = starfive_get_partition_offset(BOOT_SRC_SFC,
+								    PT_GOLDEN,
+								    IMG_TYPE_KERNEL);
+				env_set_hex("sfc_kernel_gol_part_offs", (ulong)val);
+			}
+			break;
+		default:
+			printf("Unknown argument, refer to help command...\n");
+		}
+	}
+	return CMD_RET_SUCCESS;
+}
+
 U_BOOT_LONGHELP(checkimgrcmap,
 		"[arg\n    - Check authentication status from recovery mapping\n"
 		"\tpass: 1 - spi primary\n"
@@ -149,7 +259,7 @@ U_BOOT_LONGHELP(printaprcmap,
 );
 
 U_BOOT_LONGHELP(authbm,
-		"[addr [arg ...]]\n    - authenticate image stored in memory\n"
+		"[addr [arg ...]]\n    - authenticate FIT image stored in memory\n"
 		"\tpassing arguments 'arg ...'; when verifying a Linux kernel,\n"
 #if defined(CONFIG_OF_LIBFDT)
 		"\tWhen authenticating an initrd image\n"
@@ -159,6 +269,37 @@ U_BOOT_LONGHELP(authbm,
 		"\ta third argument is required which is the address of the\n"
 		"\tdevice-tree blob.\n"
 #endif
+);
+
+U_BOOT_LONGHELP(authbimgstorage,
+		"[addr [arg ...]]\n    - authenticate image stored in storage\n"
+		"\tpassing arguments 'arg ...'; when verifying a Linux kernel,\n"
+#if defined(CONFIG_OF_LIBFDT)
+		"\tWhen authenticating an initrd image\n"
+		"\ta second argument is required which is the address of the\n"
+		"\tinitrd image. Otherwise, a '-' can be used in place\n"
+		"\tWhen authenticating a flat device-tree\n"
+		"\ta third argument is required which is the address of the\n"
+		"\tdevice-tree blob.\n"
+#endif
+);
+
+U_BOOT_LONGHELP(preosbootnotify,
+		"[arg\n    - None\n"
+);
+
+U_BOOT_LONGHELP(chksfcdualflash,
+		"[arg\n    - None\n"
+);
+
+U_BOOT_LONGHELP(getimginfo,
+		"[arg\n    - Get image storage information\n"
+		"\tpass: 1 - emmc active\n"
+		"\t      2 - emmc secondary\n"
+		"\t      3 - sfc active\n"
+		"\t      4 - sfc active\n"
+		"\t      5 - ufs active\n"
+		"\t      6 - ufs active\n"
 );
 
 U_BOOT_CMD(checkimgrcmap, CONFIG_SYS_MAXARGS, 1, do_starfive_check_img_rec_map,
@@ -171,7 +312,27 @@ U_BOOT_CMD(printaprcmap, CONFIG_SYS_MAXARGS, 1, do_starfive_print_rec_map,
 	   printaprcmap_help_text
 );
 
-U_BOOT_CMD(authbm, CONFIG_SYS_MAXARGS, 1, do_starfive_authenticate,
+U_BOOT_CMD(authbm, CONFIG_SYS_MAXARGS, 1, do_starfive_authenticate_fit_mem,
 	   "Authenticate FIT image from memory",
 	   authbm_help_text
+);
+
+U_BOOT_CMD(authbimgstorage, CONFIG_SYS_MAXARGS, 1, do_starfive_authenticate_storage,
+	   "Authenticate image in persistent storage",
+	   authbimgstorage_help_text
+);
+
+U_BOOT_CMD(preosbootnotify, CONFIG_SYS_MAXARGS, 1, do_starfive_pre_os_boot_notify,
+	   "Notify SCP before booting OS",
+	   preosbootnotify_help_text
+);
+
+U_BOOT_CMD(chksfcdualflash, CONFIG_SYS_MAXARGS, 1, do_starfive_check_sfc_dual_flash,
+	   "Check if dual SPI flash is supported",
+	   chksfcdualflash_help_text
+);
+
+U_BOOT_CMD(getimginfo, CONFIG_SYS_MAXARGS, 1, do_starfive_get_img_info,
+	   "Get Image storage information",
+	   getimginfo_help_text
 );
