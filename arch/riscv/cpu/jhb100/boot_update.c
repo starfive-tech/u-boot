f73af6516fe8023a50306862ab086dd9b4d8cc93
WeiSheng.Chng <weisheng.chng@starfivetech.com>
2025-06-25 00:01:12 +0800

    arch: riscv: cpu: jhb100: boot_update.c: Update capsule parsing utilities
    Utilities for extracting ROFS and raw image from capsule
    
    Signed-off-by: WeiSheng.Chng <weisheng.chng@starfivetech.com>

---
+++ arch/riscv/cpu/jhb100/boot_update.c
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2025 Starfive Technology International <www.starfivetech.com>
+ *
+ * Author: WeiSheng Ch'ng <weisheng.chng@starfivetech.com>
+ */
+
+#include <common.h>
+#include <mailbox.h>
+#include <dm/uclass.h>
+#include <dm/device.h>
+#include <dm/device-internal.h>
+#include <u-boot/crc.h>
+#include <malloc.h>
+#include <asm/arch/secure_vab.h>
+#include <hang.h>
+#include <misc.h>
+#include <asm/io.h>
+#include <asm/sbi.h>
+#include <asm/arch/ap_core.h>
+#include <asm/arch/boot_pti.h>
+#include <asm/arch/boot_fallback.h>
+#include <rand.h>
+
+#define CAP_PARSE_SUCCESS	0
+#define CAP_PARSE_ERROR		1
+
+/* Max size of capsule should be no more than allocated GPP size of 400 MB */
+#define UPD_CAP_MAX_SIZE	0x19000000
+
+u32 starfive_jhb100_parse_capsule(u32 *rofs_blk_size, u32 *rofs_offs, u32 load_addr)
+{
+	/* Only simple check performed by AP */
+	printf("Parsing update capsule loaded to address 0x%x\n", load_addr);
+	struct bif_image_hdr *hdr = (struct bif_image_hdr *)(uintptr_t)load_addr;
+
+	/* Check Magic */
+	if (hdr->bif_magic != BIF_MAGIC_NUM) {
+		printf("[ERROR]: hdr->bif_magic = %x\n", hdr->bif_magic);
+		return CAP_PARSE_ERROR;
+	}
+
+	/* Check Image size properties */
+	if ((!hdr->img_len) || (!hdr->img_off)) {
+		printf("[ERROR]: hdr->img_len = %x\n", hdr->img_len);
+		printf("[ERROR]: hdr->img_off = %x\n", hdr->img_off);
+		return CAP_PARSE_ERROR;
+	}
+
+	if ((hdr->img_off + hdr->img_len) > UPD_CAP_MAX_SIZE) {
+		printf("[ERROR]: hdr->img_off + hdr->img_len = %x\n",
+		       hdr->img_off + hdr->img_len);
+		return CAP_PARSE_ERROR;
+	}
+
+	/* Check sig and mft properties */
+	if ((!hdr->sign_off) || (!hdr->mft_off)) {
+		printf("[ERROR]: hdr->sign_off = %x\n", hdr->sign_off);
+		printf("[ERROR]: hdr->mft_off = %x\n", hdr->mft_off);
+		return CAP_PARSE_ERROR;
+	}
+
+	/* Check custom data properties */
+	if ((hdr->custom_data_off + hdr->custom_data_len) > UPD_CAP_MAX_SIZE) {
+		printf("[ERROR]: hdr->custom_data_off + hdr->custom_data_len = %x\n",
+		       hdr->custom_data_off + hdr->custom_data_len);
+		return CAP_PARSE_ERROR;
+	}
+
+	struct capsule_cmf *cmf = (struct capsule_cmf *)((u8 *)hdr + hdr->custom_data_off);
+
+	/* Check Magic */
+	if (cmf->magic != CMF_MAGIC_NUM) {
+		printf("[ERROR]: cmf->magic = %x\n", cmf->magic);
+		return CAP_PARSE_ERROR;
+	}
+
+	struct capsule_cap *cap = (struct capsule_cap *)((u8 *)cmf + sizeof(struct capsule_cmf));
+
+	/* Check Magic */
+	if (cap->magic != CAP_MAGIC_NUM) {
+		printf("[ERROR]: cap->magic = %x\n", cap->magic);
+		return CAP_PARSE_ERROR;
+	}
+
+	if ((cap->storage_type != CAP_STORAGE_TYPE_EMMC) &&
+	    (cap->storage_type != CAP_STORAGE_TYPE_UFS) &&
+	    (cap->storage_type != CAP_STORAGE_TYPE_SFC)) {
+		printf("[ERROR]: cap->storage_type = %x\n", cap->storage_type);
+		return CAP_PARSE_ERROR;
+	}
+
+	if (cap->count > CAPSULE_COMPONENT_MAX_COUNT) {
+		printf("[ERROR]: cap->count = %x\n", cap->count);
+		return CAP_PARSE_ERROR;
+	}
+
+	int comp_idx;
+
+	for (comp_idx = 0; comp_idx < cap->count; comp_idx++) {
+		int idx = comp_idx / CAPSULE_COMPS_PER_REG;
+		int offs = (comp_idx % CAPSULE_COMPS_PER_REG) * 8;
+
+		if (BIF_IMG_TYPE_ROFS == ((cap->img_type_maps[idx] >> offs) & 0xff)) {
+			goto extract_capsule;
+		}
+	}
+
+	printf("[ERROR]: Unknown Image Type\n");
+	return CAP_PARSE_ERROR;
+
+extract_capsule:
+	struct capsule_comp_attrs *comp_attr = (struct capsule_comp_attrs *)
+					       ((u8 *)cap + sizeof(struct capsule_cap));
+
+	comp_attr += comp_idx;
+	struct bif_image_hdr *rofs_hdr = (struct bif_image_hdr *)((u8 *)cmf +
+					 (comp_attr->off_cap));
+
+	/* Check Magic */
+	if (rofs_hdr->bif_magic != BIF_MAGIC_NUM) {
+		printf("[ERROR]: rofs_hdr->bif_magic = %x\n", rofs_hdr->bif_magic);
+		return CAP_PARSE_ERROR;
+	}
+
+	/* Check Image size properties */
+	if ((!rofs_hdr->img_len) || (!rofs_hdr->img_off)) {
+		printf("[ERROR]: rofs_hdr->img_len = %x\n", rofs_hdr->img_len);
+		printf("[ERROR]: rofs_hdr->img_off = %x\n", rofs_hdr->img_off);
+		return CAP_PARSE_ERROR;
+	}
+
+	if ((rofs_hdr->img_off + rofs_hdr->img_len) > UPD_CAP_MAX_SIZE) {
+		printf("[ERROR]: rofs_hdr->img_off + rofs_hdr->img_len = %x\n",
+		       rofs_hdr->img_off + rofs_hdr->img_len);
+		return CAP_PARSE_ERROR;
+	}
+
+	/* Check sig and mft properties */
+	if ((!rofs_hdr->sign_off) || (!rofs_hdr->mft_off)) {
+		printf("[ERROR]: rofs_hdr->sign_off = %x\n", rofs_hdr->sign_off);
+		printf("[ERROR]: rofs_hdr->mft_off = %x\n", rofs_hdr->mft_off);
+		return CAP_PARSE_ERROR;
+	}
+
+	*rofs_blk_size = (rofs_hdr->img_len % MMC_BLK_SIZE) ?
+			 ((rofs_hdr->img_len / MMC_BLK_SIZE) + 1) :
+			 (rofs_hdr->img_len / MMC_BLK_SIZE);
+	*rofs_offs = hdr->custom_data_off + comp_attr->off_cap + load_addr +
+		     BIF_HDR_LENGTH + BIF_MFT_LENGTH + BIF_SIG_LENGTH;
+
+	printf("[SUCCESS]: rofs_blk_size = 0x%x\n", *rofs_blk_size);
+	printf("[SUCCESS]: rofs_offs in memory is 0x%x\n", *rofs_offs);
+
+	return CAP_PARSE_SUCCESS;
+}
--- arch/riscv/include/asm/arch-jhb100/secure_vab.h
+++ arch/riscv/include/asm/arch-jhb100/secure_vab.h
@@ -12,6 +12,28 @@
 #include <linux/stddef.h>
 #include <linux/bitops.h>
 
+#define CAPSULE_COMPONENT_MAX_COUNT	(32)
+#define CAPSULE_COMPS_PER_REG		(4)
+#define BIF_HDR_LENGTH			0x200
+#define BIF_MFT_LENGTH			0xE00
+#define BIF_SIG_LENGTH			0x200
+#define CAP_STORAGE_TYPE_EMMC		0x01
+#define CAP_STORAGE_TYPE_UFS		0x02
+#define CAP_STORAGE_TYPE_SFC		0x03
+
+#define BIF_IMG_TYPE_L0			0x0
+#define BIF_IMG_TYPE_L1			0x1
+#define BIF_IMG_TYPE_SPL		0x2
+#define BIF_IMG_TYPE_OPENSBI		0x3
+#define BIF_IMG_TYPE_UBOOT_PROP		0x4
+#define BIF_IMG_TYPE_UBOOT_DTB		0x5
+#define BIF_IMG_TYPE_LINUX_KERNEL	0x6
+#define BIF_IMG_TYPE_LINUX_KERNEL_DTB	0x7
+#define BIF_IMG_TYPE_ROFS		0x8
+#define BIF_IMG_TYPE_FILTER_CONFIG	0x9
+#define BIF_IMG_TYPE_RAW_IMG		0xA
+#define BIF_IMG_TYPE_CAPSULE		0xFE
+
 struct bif_image_hdr {
 	u32 bif_magic;		// 0x0 BIF Magic Number. ("BIF\0", 0x42494600)
 #define BIF_MAGIC_NUM					(0x00464942)
@@ -32,7 +54,116 @@ struct bif_image_hdr {
 	u32 img_len;
 	u32 img_off;
 	u32 sign_off;		// 0x20 Offset to Signature of this hdr + image data
-	u32 mft_off;            // 0x24 Offset to Key Manifest
-	u8 reserved_0[472];
+	u32 mft_off;		// 0x24 Offset to Key Manifest
+	u32 custom_data_len;
+	u32 custom_data_off;
+	u8 reserved_0[464];
 };
+
+struct key_mft {
+	u32 mft_magic;		// 0x0 Key Manifest Magic Number. ("MFT\0", 0x4d465400)
+#define MFT_MAGIC_NUM		(0x0054464d)
+	u32 version;		// 0x4 Version:
+				//   [7:0] spec version = 0x02
+#define GET_MFT_SPEC_VERSION(val)			(((val) >> 0) & 0xFF)
+#define   SUPPORTED_MFT_SPEC_VERSION			(0x2)
+	u32 sflags;		// 0x8 Sign Control Flags:
+				//   [29:28] Device Public Key Type (0-ECDSA384, 1-SM2, 2-RSA4096)
+				//   [25:24] Device Public Key Table ID (11b reserved)
+				//   [21:20] Device Public Key Table Key Count - 1 (0 base)
+				//   [17:16] Device Public Key ID
+				//   [9:8] Image Public Key Type (0-ECDSA384, 1-SM2, 2-RSA4096)
+				//   [6:0] Image Public Key ID
+#define GET_DEVICE_PUBLIC_KEY_TYPE(val)			(((val) >> 28) & 0x3)
+#define   DEVICE_PUBLIC_KEY_ECDSA384			(0x0)
+#define   DEVICE_PUBLIC_KEY_SM2				(0x1)
+#define   DEVICE_PUBLIC_KEY_RSA4096			(0x2)
+#define GET_DEVICE_PUBLIC_KEY_TABLE_ID(val)		(((val) >> 24) & 0x3)
+#define   MAX_DEVICE_PUBLIC_KEY_TABLE_NUM		(0x3)
+#define GET_DEVICE_PUBLIC_KEY_TABLE_KEY_COUNT(val)	((((val) >> 20) & 0x3) + 1)
+#define GET_DEVICE_PUBLIC_KEY_ID(val)			(((val) >> 16) & 0x3)
+#define GET_IMAGE_PUBLIC_KEY_TYPE(val)			(((val) >> 8) & 0x3)
+#define   IMAGE_PUBLIC_KEY_TYPE_ECDSA384		(0x0)
+#define   IMAGE_PUBLIC_KEY_TYPE_SM2			(0x1)
+#define   IMAGE_PUBLIC_KEY_TYPE_RSA4096			(0x2)
+#define GET_IMAGE_PUBLIC_KEY_ID(val)			(((val) >> 0) & 0x7F)
+	u32 eflags;		// 0xC Encryption Control Flags:
+				//   [29:28] Encryption Type (0-AES-256-CBC, 1-SM4-CBC)
+				//   [25:24] Encryption Key ID (FWkey, 10b 11b reserved)
+#define GET_ENCRYPTION_TYPE(val)			(((val) >> 28) & 0x3)
+#define   ENCRYPTION_TYPE_AES256			(0x0)
+#define   ENCRYPTION_TYPE_SM4				(0x1)
+#define GET_ENCRYPTION_KEY_ID(val)			(((val) >> 24) & 0x3)
+#define   MAX_ENCRYPTION_KEY_NUM			(0x2)
+	u32 img_ver_revoke_loc; // 0x10 OTP addr of Image version revocation
+	u8 reserved_0[204];
+	u8 iza[32];		// 0xE0 Za of Image Signer, for SM2 only
+	u8 reserved_1[32];
+	u8 dza[32];		// 0x120 Za of Device Signer, for SM2 only
+	u8 reserved_2[32];
+	u8 iv[16];
+	u8 reserved_3[16];
+	u8 ekek[32];		// 0x180 Encrypted Key Encryption Key (by FWkey, no padding)
+	u8 reserved_4[32];
+	u8 edek[32];		// 0x1C0 Encrypted Data Encryption Key (by KEK, no padding)
+	u8 reserved_5[32];
+	u8 eipk[512];		// 0x200 Encrypted Image Public Key (by DEK, no padding)
+#define MAX_IMAGE_PUBLIC_KEY_SIZE			(0x200)
+	u8 edpkt[2048];		// 0x400 Encrypted Device Public Key Table (by DEK, no padding)
+#define DEVICE_PUBLIC_KEY_ENTRY_SIZE			(0x200)
+};
+
+struct capsule_comp_attrs {
+	u32 off_cap;
+	u32 off_storage;
+	u32 size;
+};
+
+struct capsule_cap {
+	u32 magic;
+#define CAP_MAGIC_NUM	(0x00504143)
+	u32 len;
+	u8 desc[64];
+	u32 storage_type;
+	u32 image_sfc_cs;
+	u32 count;
+	u32 img_type_maps[CAPSULE_COMPONENT_MAX_COUNT / CAPSULE_COMPS_PER_REG];
+};
+
+struct capsule_rpt {
+	u32 magic;
+#define RPT_MAGIC_NUM	(0x00545052)
+	u32 len;
+	u32 enc_rev_bit_msk;
+	u32 pkh_rev_bit_msk;
+	u32 pkid_rev_bit_msk;
+	u32 pkh_img_key_rev_bit_msk[4 * 3];
+	u32 svn_rev_attrs[CAPSULE_COMPONENT_MAX_COUNT];
+	u8 rsvd[148];
+};
+
+struct capsule_cmf {
+	u32 magic;
+#define CMF_MAGIC_NUM	(0x00464D43)
+	u32 version;
+	u32 cflag;
+	u32 body_len;
+};
+
+struct capsule_info {
+	struct bif_image_hdr hdr;
+	struct key_mft mft;
+	struct capsule_cmf cmf;
+	struct capsule_cap cap;
+	struct capsule_comp_attrs attrs[CAPSULE_COMPONENT_MAX_COUNT];
+	struct capsule_rpt rpt;
+	u8 cus_sig[512];	// custom data signature
+	u8 cap_sig[512];	// capsule signature
+	u8 mft_sig[512];
+
+	u32 part_capsule_off;
+};
+
+u32 starfive_jhb100_parse_capsule(u32 *rofs_blk_size, u32 *rofs_offs, u32 load_add);
+
 #endif /* _SECURE_VAB_H_ */
