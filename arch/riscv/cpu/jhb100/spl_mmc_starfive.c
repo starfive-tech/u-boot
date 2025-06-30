7df818eee55a5eb2fc75c405e8f3762cf596f533
WeiSheng.Chng <weisheng.chng@starfivetech.com>
2025-06-30 23:43:13 +0800

    arch: riscv: cpu: jhb100: spl_mmc_starfive.c: Handles eMMC boot retry
    Handles eMMC boot retry during AP hang
    
    Signed-off-by: WeiSheng.Chng <weisheng.chng@starfivetech.com>

--- arch/riscv/cpu/jhb100/spl_mmc_starfive.c
+++ arch/riscv/cpu/jhb100/spl_mmc_starfive.c
@@ -291,6 +291,7 @@ static int spl_mmc_load_image_handler(struct spl_image_info *spl_image,
 					    FB_RCV_SPL_SET_UBOOT_PROP_CLEAR_MSK,
 					    SET);
 		starfive_set_fb_rec_map(fb_rec_mmc);
+		starfive_add_ap_sts_retry_cnt(BOOTSTG_U_BOOT_PROPER, BOOT_TRIAL_CNT);
 	}
 
 	return starfive_spl_mmc_load_image(spl_image, bootdev, fb_rec_mmc);
