350c7ac8a4dabaed9ae487fb470a2605e394dfd1
Hal Feng <hal.feng@starfivetech.com>
2025-07-14 10:22:19 +0800

    configs: starfive: visionfive2: Add fdt_conf_set
    Add fdt_conf env variable to identify VF2, VF2 CM and VF2 Lite fdt configuration.
    
    Signed-off-by: Hal Feng <hal.feng@starfivetech.com>

--- include/configs/starfive-visionfive2.h
+++ include/configs/starfive-visionfive2.h
@@ -232,6 +232,16 @@
 	"run visionfive2_mem_set;"		\
 	"run chipa_set;\0"
 
+#define FDT_CONF_SET					\
+	"fdt_conf_set="					\
+	"if test ${vf2_board_type} = 1; then "		\
+		"setenv fdt_conf config-cm;"		\
+	"elif test ${vf2_board_type} = 2; then "	\
+		"setenv fdt_conf config-lite;"		\
+	"else "						\
+		"setenv fdt_conf config-default;"	\
+	"fi; \0"
+
 #define VF2_SDK_BOOTENV			\
 	"bootenv=uEnv.txt\0"		\
 	"bootenv_sdk=vf2_uEnv.txt\0"	\
@@ -383,6 +393,7 @@
 	CPU_FREQ_VOL_SET				\
 	CMA_SIZE_SET					\
 	VISIONFIVE2_MEM_SET				\
+	FDT_CONF_SET					\
 	"type_guid_gpt_loader1=" TYPE_GUID_LOADER1 "\0" \
 	"type_guid_gpt_loader2=" TYPE_GUID_LOADER2 "\0" \
 	"type_guid_gpt_system=" TYPE_GUID_SYSTEM "\0"	\
