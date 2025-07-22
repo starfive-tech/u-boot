325c579112de84cf51940c4cb0bcbb0eca84acf3
zhiwei.lim <zhiwei.lim@starfivetech.com>
2025-07-22 12:08:05 +0800

    drivers: mmc: [WORKAROUND] Remove block limit that slow the transfer on JHB100
    Remove the block limit that lower the transfer rate on JHB100. Revisit
    these changes after power on stage done.
    
    Signed-off-by: zhiwei.lim <zhiwei.lim@starfivetech.com>

--- drivers/mmc/rockchip_sdhci.c
+++ drivers/mmc/rockchip_sdhci.c
@@ -517,19 +517,21 @@ static int sdhci_set_ios_post(struct sdhci_host *host)
 
 	sdhci_writew(host, reg, DWCMSHC_EMMC_EMMC_CTRL);
 
+	/* TODO: This block limit was removed to avoid throttling the transfer rate
+	   on JHB100. Revisit after power-on stage done */
 	/*
 	 * Reading more than 4 blocks with a single CMD18 command in PIO mode
 	 * triggers Data End Bit Error using a slower mode than HS200. Limit to
 	 * reading max 4 blocks in one command when using PIO mode.
 	 */
-	if (!(host->flags & USE_DMA)) {
-		if (mmc->selected_mode == MMC_HS_200 ||
-		    mmc->selected_mode == MMC_HS_400 ||
-		    mmc->selected_mode == MMC_HS_400_ES)
-			cfg->b_max = CONFIG_SYS_MMC_MAX_BLK_COUNT;
-		else
-			cfg->b_max = 4;
-	}
+	// if (!(host->flags & USE_DMA)) {
+	// 	if (mmc->selected_mode == MMC_HS_200 ||
+	// 	    mmc->selected_mode == MMC_HS_400 ||
+	// 	    mmc->selected_mode == MMC_HS_400_ES)
+	// 		cfg->b_max = CONFIG_SYS_MMC_MAX_BLK_COUNT;
+	// 	else
+	// 		cfg->b_max = 4;
+	// }
 
 	return 0;
 }
