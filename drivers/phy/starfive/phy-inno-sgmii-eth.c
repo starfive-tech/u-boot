be81b60f03c1d439a4576ea584b8f56b523aa308
John Chew <yuinyee.chew@starfivetech.com>
2025-08-18 09:07:06 +0000

    drivers: phy: starfive: Add innosilicon phy driver
    Add innosilicon phy initialization handling using generic phy
    uclass. When turning on this phy, it is require to wait until
    PLL LOCK STATUS is release.
    
    Signed-off-by: John Chew <yuinyee.chew@starfivetech.com>

---
+++ drivers/phy/starfive/phy-inno-sgmii-eth.c
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2018 Rockchip Electronics Co. Ltd.
+ *
+ * Author: Wyon Bi <bivvy.bi@rock-chips.com>
+ */
+
+#include <dm.h>
+#include <dm/device_compat.h>
+#include <generic-phy.h>
+#include <linux/iopoll.h>
+#include <linux/clk-provider.h>
+#include <linux/delay.h>
+
+#define PLL_LOCK_STATUS		0x13c0
+#define PLL_IS_LOCK			BIT(0)
+#define PHY_POLL_TIMEOUT_US	10000
+
+struct inno_sgmii_phy_data {
+	struct clk_bulk clks;
+	unsigned int phy_status_offset;
+	unsigned int phy_status_mask;
+	unsigned int phy_status_valid;
+};
+
+static int inno_sgmii_phy_exit(struct phy *phy)
+{
+	struct udevice *dev = phy->dev;
+	struct inno_sgmii_phy_data *data = dev_get_priv(dev);
+
+	clk_disable_bulk(&data->clks);
+
+	return 0;
+}
+
+static int inno_sgmii_phy_configure(struct phy *phy, void *params)
+{
+	u32 val;
+	int ret;
+	void *phy_base = dev_read_addr_ptr(phy->dev);
+
+	if (phy_base) {
+		/* Wait for SGMII Internal PHY to be READY */
+		ret = readl_poll_timeout(phy_base + PLL_LOCK_STATUS, val,
+								(val & PLL_IS_LOCK), PHY_POLL_TIMEOUT_US);
+		if (ret) {
+			printf("%s sgmii timeout\n", __func__);
+			return ret;
+		}
+	} else {
+		printf("Failed to read sgmii phy base\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static const struct phy_ops inno_sgmii_phy_ops = {
+	.configure = inno_sgmii_phy_configure,
+	.exit = inno_sgmii_phy_exit,
+};
+
+static int inno_sgmii_phy_probe(struct udevice *dev)
+{
+	struct inno_sgmii_phy_data *data;
+	int ret;
+
+	data = calloc(1, sizeof(struct inno_sgmii_phy_data));
+	if (!data)
+		return -ENOMEM;
+
+	ret = clk_get_bulk(dev, &data->clks);
+	if (ret < 0)
+		return ret;
+
+	ret = clk_enable_bulk(&data->clks);
+	if (ret) {
+		printf("%s sgmii clk enable failed\n", __func__);
+		return ret;
+	}
+
+	return 0;
+}
+
+static const struct udevice_id inno_sgmii_phy_of_match[] = {
+	{
+		.compatible = "starfive,dwmac-inno-sgmii-phy",
+	},
+	{}
+};
+
+U_BOOT_DRIVER(dwmac_sgmii_phy_driver) = {
+	.name = "starfive-dwmac-sgmii-phy",
+	.id = UCLASS_PHY,
+	.of_match = inno_sgmii_phy_of_match,
+	.probe = inno_sgmii_phy_probe,
+	.ops = &inno_sgmii_phy_ops,
+	.priv_auto = sizeof(struct inno_sgmii_phy_data),
+};
