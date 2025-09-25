308cb8c425f596fc39244ff8166d9ebfb9cd5866
John Chew <yuinyee.chew@starfivetech.com>
2025-09-25 03:17:57 +0000

    cmd: ufs.c: Add UFS command support
    Added command:
    1. ufs list
    2. ufs create
    3. ufs update
    4. ufs remove
    
    Signed-off-by: John Chew <yuinyee.chew@starfivetech.com>

--- cmd/ufs.c
+++ cmd/ufs.c
@@ -8,10 +8,29 @@
 #include <common.h>
 #include <command.h>
 #include <ufs.h>
+#include <dm.h>
+
+#include <ufs.h>
+
+static int do_ufs_get_device(struct udevice **dev, u8 index)
+{
+	int ret;
+
+	ret = uclass_get_device(UCLASS_UFS, index, dev);
+	if (ret)
+		return CMD_RET_SUCCESS;
+
+	ret = ufs_probe_dev(index);
+	if (ret)
+		return CMD_RET_FAILURE;
+
+	return uclass_get_device(UCLASS_UFS, index, dev);
+}
 
 static int do_ufs(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	int dev, ret;
+	struct udevice *ufs_dev;
 
 	if (argc >= 2) {
 		if (!strcmp(argv[1], "init")) {
@@ -25,13 +44,84 @@ static int do_ufs(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 			}
 
 			return CMD_RET_SUCCESS;
+
+		} else if (!strcmp(argv[1], "list")) {
+			if (do_ufs_get_device(&ufs_dev, 0))
+				return CMD_RET_FAILURE;
+
+			ufs_list_lus(ufs_dev);
+
+			return CMD_RET_SUCCESS;
+
+		} else if (!strcmp(argv[1], "create")) {
+			if (argc < 5)
+				return CMD_RET_USAGE;
+
+			u8 lun = dectoul(argv[2], NULL);
+			u32 size = dectoul(argv[3], NULL);
+			u32 attr = dectoul(argv[4], NULL);
+
+			if (!size) {
+				printf("Size cannot be 0!\n");
+				return CMD_RET_FAILURE;
+			}
+
+			if (do_ufs_get_device(&ufs_dev, 0))
+				return CMD_RET_FAILURE;
+
+			if (ufs_create_lu(ufs_dev, lun, size, attr)) {
+				printf("LUN create failure\n");
+				return CMD_RET_FAILURE;
+			}
+
+			return CMD_RET_SUCCESS;
+
+		} else if (!strcmp(argv[1], "update")) {
+			if (argc < 5)
+				return CMD_RET_USAGE;
+
+			u8 lun = dectoul(argv[2], NULL);
+			u32 size = dectoul(argv[3], NULL);
+			u32 attr = dectoul(argv[4], NULL);
+
+			if (!size) {
+				printf("Size cannot be 0!\n");
+				return CMD_RET_FAILURE;
+			}
+
+			if (do_ufs_get_device(&ufs_dev, 0))
+				return CMD_RET_FAILURE;
+
+			if (ufs_update_lu(ufs_dev, lun, size, attr)) {
+				printf("LUN update failure\n");
+				return CMD_RET_FAILURE;
+			}
+
+			return CMD_RET_SUCCESS;
+
+		} else if (!strcmp(argv[1], "remove")) {
+			if (argc == 3) {
+				if (do_ufs_get_device(&ufs_dev, 0))
+					return CMD_RET_FAILURE;
+				dev = dectoul(argv[2], NULL);
+				return ufs_remove_lu(ufs_dev, dev);
+			}
 		}
 	}
 
 	return CMD_RET_USAGE;
 }
 
-U_BOOT_CMD(ufs, 3, 1, do_ufs,
-	   "UFS sub-system",
-	   "init [dev] - init UFS subsystem\n"
+U_BOOT_CMD(ufs, 6, 1, do_ufs,
+	"UFS sub-system",
+	"init [dev]  - init UFS subsystem\n"
+	"ufs list  - list existing Logical Units\n"
+	"ufs create <lun> <size> <attr>  - create a logical unit\n"
+	"   Size 1 == 4MB\n"
+	"   Attribute[0:1] - 0: Not bootable, 1:Boot LU A, 2: Boot LU B\n"
+	"ufs update <lun> <size> <attr>  - create a logical unit\n"
+	"   Size 1 == 4MB\n"
+	"   Attribute[0:1] - 0: Not bootable, 1:Boot LU A, 2: Boot LU B\n"
+	"ufs remove <lun>  - remove a logical unit\n"
+	"ATTENTION: create, update, and remove will reset data in all LUNs\n"
 );
--- include/ufs.h
+++ include/ufs.h
@@ -29,4 +29,57 @@ int ufs_probe_dev(int index);
  * Return: 0 if Ok, -ve on error
  */
 int ufs_scsi_bind(struct udevice *ufs_dev, struct udevice **scsi_devp);
+
+/**
+ * ufs_list_lus() - List all configured Logical Units (LUs) in a UFS device
+ *
+ * @ufs_dev: Pointer to the UFS device
+ *
+ * This function enumerates and prints information about all existing LUs
+ * (Logical Units) on the specified UFS device.
+ */
+void ufs_list_lus(struct udevice *ufs_dev);
+
+/**
+ * ufs_create_lu() - Create a new Logical Unit (LU) on a UFS device
+ *
+ * @ufs_dev: Pointer to the UFS device
+ * @lun:     Logical Unit Number to assign (0-7)
+ * @size:    Size of the LU in blocks (4MB per block)
+ * @attr:    Attributes for the LU (e.g., bootable, read-only flags)
+ *
+ * This function creates a new LU on the specified UFS device with the given
+ * size and attributes.
+ *
+ * Return: 0 on success, -ve on error
+ */
+int ufs_create_lu(struct udevice *ufs_dev, u8 lun, u32 size, u32 attr);
+
+/**
+ * ufs_update_lu() - Modify an existing Logical Unit (LU) on a UFS device
+ *
+ * @ufs_dev: Pointer to the UFS device
+ * @lun:     Logical Unit Number of the LU to update
+ * @size:    New size to set for the LU
+ * @attr:    New attributes to set
+ *
+ * This function updates the size and/or attributes of an existing LU
+ * on the specified UFS device.
+ *
+ * Return: 0 on success, -ve on error
+ */
+int ufs_update_lu(struct udevice *ufs_dev, u8 lun, u32 size, u32 attr);
+
+/**
+ * ufs_remove_lu() - Remove a Logical Unit (LU) from a UFS device
+ *
+ * @ufs_dev: Pointer to the UFS device
+ * @lun:     Logical Unit Number to remove
+ *
+ * This function removes a specified LU from the UFS device.
+ *
+ * Return: 0 on success, -ve on error
+ */
+int ufs_remove_lu(struct udevice *ufs_dev, int lun);
+
 #endif
