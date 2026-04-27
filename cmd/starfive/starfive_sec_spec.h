1eef901b3b4ac84611479430c6a41e09ac97c396
Joshua Yeong <joshua.yeong@starfivetech.com>
2026-04-27 17:05:39 +0800

    mpxy: Add SET_SFC_BOOT_INSTANCE secure service
    Add sercure service that switches SFC CS instance
    for dual flash solution.
    
    Signed-off-by: Joshua Yeong <joshua.yeong@starfivetech.com>

--- cmd/starfive/starfive_sec_spec.h
+++ cmd/starfive/starfive_sec_spec.h
@@ -328,6 +328,17 @@ static const struct request_spec request_specs[] = {
 		.has_external_data = 0
 	},
 	{
+		.request_id = 30,
+		.request_name = "set_sfc_boot_instance",
+		.param_count = 1,
+		.param_names = {"flags"},
+		.resp_count = 1,
+		.resp_names = {"status"},
+		.resp_size = 4,
+		.need_auth = 0,
+		.has_external_data = 0
+	},
+	{
 		.request_id = 513,
 		.request_name = "dice_prov_cert0",
 		.param_count = 1,
